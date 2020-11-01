526271d76903143df73d156129a01c1e79d0e058
Nicolai Stoianov <ston1x@PowerBook.local>
2020-11-01 17:57:31 +0200

    Initial commit

---
+++ lib/pointmd_comments.rb
@@ -0,0 +1,6 @@
+require "pointmd_comments/version"
+
+module PointmdComments
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pointmd_comments/version.rb
@@ -0,0 +1,3 @@
+module PointmdComments
+  VERSION = "0.1.0"
+end
